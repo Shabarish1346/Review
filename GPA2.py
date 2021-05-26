6e02f5c32764ab87b353bf2d5db964c881ecf5d0
sana srikar <srikarsana1995@gmail.com>
2021-05-26 18:36:00 +0530

    Update GPA2.py
    cleaned the code

--- GPA2.py
+++ GPA2.py
@@ -1,30 +1,37 @@
 #GPA2
 
 #finding the prime numbers from 3 to given input and appending it in prime list
-for i in range(3,num,2):
-  flag = True
-  for j in range(2,i):
-    if i%j == 0:
-      flag = False
-      break
-  if flag:
-    prime.append(i)
+def FindingPrimeNumbersFrom3toN(N):
+    prime = list()
+    for i in range(3,num,2):
+        flag = True
+        for j in range(2,i):
+            if i%j == 0:
+                flag = False
+                break
+            if flag:
+                prime.append(i)
+    return prime
+
+
 
 #checking the primes are divisible by given input
-flag = False
-for i in prime:
-  if num%i == 0:
-    flag = True
-    print(i)
+def PrintPrimeFactors(primes_list,number):
+    IsPrimeFactorFound = False  #always use a question for booleans. Easy to understand
+    for i in primes_list:
+        if num%i == 0:
+            IsPrimeFactorFound = True
+            print(i)
+    if IsPrimeFactorFound == FALSE:
+        print(num)
+  
+  
 
-#If no prime factors are there, the given input is getting printed
-if not flag:
-  print(num)
-  
-  
 # equivalent to main function in c  
 if __name__ == "__main__":
+
     num = int(input())
-    prime = [2]
- 
+    primenumbers_list = FindingPrimeNumbersFrom3toN(num)
+    PrintPrimeFactors(primenumbers_list,num)
+
  
